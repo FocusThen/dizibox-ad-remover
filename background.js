7d5e762059f1ac5fa2e3bf0aeb6e277ec999e429
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2020-04-18 22:24:43 +0300

    trying to on & off button 🛠

--- background.js
+++ background.js
@@ -1,1 +1,18 @@
 console.log("Created By FocusThen. Github ==> https://github.com/FocusThen");
+
+// let toggle = false;
+// window.word = "off";
+// chrome.runtime.onMessage.addListener(receiver);
+// toggle = !toggle;
+// function receiver(request, sender, sendResponse) {
+//   if (request.text == "toggle") {
+//     toggle = !toggle;
+//     if (toggle) {
+//       window.word = "on";
+//       chrome.tabs.executeScript(tab.id, { file: "CSS.main.css" });
+//     } else {
+//       window.word = "off";
+//       chrome.tabs.executeScript(null, { code: "alert()" });
+//     }
+//   }
+// }
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Created By FocusThen. Github ==> https://github.com/FocusThen");
+
+// const btn = document.getElementById("btn");
+// let message = {
+//   text: "toggle",
+// };
+
+// let bgpage = chrome.extension.getBackgroundPage();
+// btn.textContent = bgpage.word;
+
+// btn.addEventListener("click", () => {
+//   chrome.runtime.sendMessage(message);
+//   window.close();
+// });
